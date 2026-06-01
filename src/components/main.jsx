75d1403540016409cc7d7517e584629b6a4383ad
M.Azam <thegreenguy48@gmail.com>
2026-06-01 17:27:03 +0500

    latest v2

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react";
 import '/src/index.css'
 import Navbar from './Navbar.jsx'
 import Home from './Home.jsx'
@@ -37,7 +36,6 @@ createRoot(document.getElementById('root')).render(
     <footer>
       <Footer />
     </footer>
-<Analytics />
     </div>
   </StrictMode>
 )
